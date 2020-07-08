ce913118db4ffaa743c2c16fa2bdf13f5cf68dbf
danmalowany <dan.malowany@gmail.com>
2020-07-08 14:37:12 +0300

    Added support for various backbones in FasterRCNN

--- models/detection/FasterRCNN/faster_rcnn_model.py
+++ models/detection/FasterRCNN/faster_rcnn_model.py
@@ -1,6 +1,16 @@
-from torchvision.models.detection import fasterrcnn_resnet50_fpn
+from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
 
 
-def get_fasterrcnn(num_classes):
-    model = fasterrcnn_resnet50_fpn(pretrained_backbone=True, num_classes=num_classes)
+def get_fasterrcnn(num_classes, backbone_name='resnet50'):
+    if backbone_name not in ['resnet50', 'resnet101', 'resnet152']:
+        raise ValueError('Only "resnet50", "resnet101" and "resnet152" are supported backbone names')
+
+    model = fasterrcnn_fpn(backbone=backbone_name, pretrained_backbone=True, num_classes=num_classes)
+    return model
+
+
+def fasterrcnn_fpn(backbone='resnet50', num_classes=91, pretrained_backbone=True, **kwargs):
+    backbone = resnet_fpn_backbone(backbone, pretrained_backbone)
+    model = FasterRCNN(backbone, num_classes, **kwargs)
     return model
